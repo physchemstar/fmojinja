781aac8bf1aada73344713c1300b3a9bc08dd174
Yusuke Kawashima <y-kawashima@hoshi.ac.jp>
2021-06-02 14:01:38 +0900

    add rmsd command

--- fmojinja/cpptraj/__main__.py
+++ fmojinja/cpptraj/__main__.py
@@ -1,8 +1,10 @@
 from ..mixin import SubCommands
 from .snapshot import Snapshot
 from .hbond import Hbond
+from .rmsd import Rmsd
 
 SubCommands.main_proc({
     "snapshot": Snapshot,
     "hbond": Hbond,
+    "rmsd": Rmsd
 })
