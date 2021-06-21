41282ddfdeaf6c0a03dca24773b590525aaeeece
Yusuke Kawashima <y-kawashima@hoshi.ac.jp>
2021-06-21 14:45:42 +0900

    change the restraint method ibelly to ntr for pmemd execution.

--- fmojinja/sander/opth.py
+++ fmojinja/sander/opth.py
@@ -11,18 +11,22 @@ class OptH(TemplateRendererMixin):
 OptH
 &cntrl
   imin=1, 
-  maxcyc=100000, 
-  ncyc=3000, 
-  drms=0.1,
-  ibelly=1
-  bellymask='!@H='
-  ig=-1,
-  vlimit=-1,
-  iwrap=1,
+  maxcyc={{ maxcyc }}, 
+  ncyc=3000,
+  drms={{ drms }},
+  ntr=1,
+  restraintmask='!@H=',
+  restraint_wt={{ restraint_wt }},
+  ig={{ seed }}
 /
 
 """
 
     @classmethod
     def set_arguments(cls, p):
-        return super(cls, cls).set_arguments(p)
+        p.add_argument("-mc", "--maxcyc", default=10000)
+        p.add_argument("--drms", default=1e-4)
+        p.add_argument("-rw", "--restraint-wt", default=1000)
+        p.add_argument("-ig", "--seed", default=-1)
+
+        return super().set_arguments(p)
