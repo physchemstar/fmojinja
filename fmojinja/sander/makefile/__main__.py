2ae80de6e9a077ff413572725138fc4e3d0839a1
Yusuke Kawashima <y-kawashima@hoshi.ac.jp>
2021-07-20 17:31:54 +0900

    add snapmin

--- fmojinja/sander/makefile/__main__.py
+++ fmojinja/sander/makefile/__main__.py
@@ -1,10 +1,10 @@
 from ...mixin import SubCommands
-from .step_min import StepMin
+from .snap_min import SnapMin
 from .step_prep import StepPrep
 from .step_prod import StepProd
 
 SubCommands.main_proc({
-    "step_min": StepMin,
+    "snap_min": SnapMin,
     "step_prep": StepPrep,
     "step_prod": StepProd
 })
