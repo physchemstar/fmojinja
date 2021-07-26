9aec4c1de2687773bbf3b97c57b237459127c600
Yusuke Kawashima <y-kawashima@hoshi.ac.jp>
2021-07-26 20:52:19 +0900

    fix

--- fmojinja/cpptraj/reader/rmsd_reader.py
+++ fmojinja/cpptraj/reader/rmsd_reader.py
@@ -1,3 +1,5 @@
+import os.path
+
 import pandas as pd
 from ...mixin import ReaderMixin
 import re
@@ -12,6 +14,13 @@ class RmsdReaderMixin(ReaderMixin):
         widths = [len(i) for i in re.findall(" *[^ ]+", header)]
         widths = [8] + widths
         df = pd.read_fwf(path, widths=widths)
+        if inpcrd_names is None:
+            df = df.rename({'#Frame': 'step'}, axis=1)
+        else:
+            frame2step = []
+            for path in inpcrd_names:
+                frame2step.append(int(re.findall("[0-9]+", os.path.basename(path))[0]))
+            df = df.assign(step=lambda d: [frame2step[i - 1] for i in d.step])
         align_mask = [i for i in df.columns.values if i.startswith("rmsd_align")]
         df = df.rename({align_mask[0]: align_mask[0].replace("align", "")}, axis=1)
         df = df.assign(align_mask=align_mask[0].replace("rmsd_align", ""))
