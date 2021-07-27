918e613ecf48cdfdf35e57a4426f6dee42b8d49d
Yusuke Kawashima <kyusque@users.noreply.github.com>
2021-07-27 15:36:01 +0900

    Update snapshot.py

--- fmojinja/cpptraj/snapshot.py
+++ fmojinja/cpptraj/snapshot.py
@@ -16,7 +16,7 @@ align {{ align_mask }} {{ "ref {}".format(ref) if ref else "first" }}
 {% for path in trajin %}
 trajout {{ prefix ~ path.stem }}.rst onlyframes {{ loop.index }}
 {% endfor %}
-parmwrite out {{ prefix }}.prmtop
+parmwrite out {{ prefix }}prmtop
 run
 """
 
