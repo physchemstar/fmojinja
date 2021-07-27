aeab40843a5af2a6c07d678860509ece82593f64
Yusuke Kawashima <y-kawashima@hoshi.ac.jp>
2021-07-27 18:51:33 +0900

    update docstring

---
+++ fmojinja/abinitmp/__init__.py
@@ -0,0 +1,10 @@
+"""ABINIT-MP input file (.ajf) generator.
+
+ABINIT-MP is a Fragment Molecular Orbital method software for high-performance computer.
+
+    - FMO Drug Design Consortium
+        - https://fmodd.jp/top-en/
+    - ABINIT-MP Open Series
+        - http://www.cenav.org/abinit-mp-open_ver-1-rev-22/
+
+"""
