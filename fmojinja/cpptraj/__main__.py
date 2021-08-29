ad0cc05740232681f17cad6a672aada9614b2cb9
Yusuke Kawashima <y-kawashima@hoshi.ac.jp>
2021-08-29 20:03:19 +0900

    update

--- fmojinja/cpptraj/__main__.py
+++ fmojinja/cpptraj/__main__.py
@@ -3,10 +3,12 @@ from .snapshot import Snapshot
 from .hbond import Hbond
 from .rmsd import Rmsd
 from .reduce_frames import ReduceFrames
+from .split import Split
 
 SubCommands.main_proc({
     "snapshot": Snapshot,
     "hbond": Hbond,
     "rmsd": Rmsd,
     "reduce_frames": ReduceFrames,
+    "split": Split
 })
--- fmojinja/jinja_filters.py
+++ fmojinja/jinja_filters.py
@@ -1,7 +1,8 @@
-from typing import List, Any
+import string
+from typing import List, Any, Callable
 
 
-def broadcast_n(n: int):
+def broadcast_n(n: int) -> Callable[[List[Any], int], Any]:
     def broadcast(values: List[Any], onedex: int) -> Any:
         if len(values) == 1:
             return values[0]
@@ -9,3 +10,7 @@ def broadcast_n(n: int):
             raise ValueError(f"The numbers doesn't match. expected {n} or 1, exact {len(values)}.")
         return values[onedex - 1]
     return broadcast
+
+
+def digit2letter(n: int) -> str:
+    return string.ascii_letters[len(str(n)) - 1]
